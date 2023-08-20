2d3c9b8a3ca79b1bc5c5a95d0120d2a319fb3ca4
kunal raj shah <rajkunal7279@gmail.com>
2023-08-20 20:28:39 +0530

    make user can change Password

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,6 +1,46 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useRef, useContext } from "react";
+import AuthContext from "../Components/Store/AuthContext";
 
 const Profile = () => {
+  const newPassword = useRef("");
+  const AuthCtxt = useContext(AuthContext);
+
+  const SubmitHandler = async (event) => {
+    event.preventDefault();
+    let newInputPassword = newPassword.current.value;
+
+    try {
+      const Response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBOXHgDlb77PEn3G_YXmJvzMx620ExFDuI",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            idToken: AuthCtxt.token,
+            password: newInputPassword,
+            returnSecureToken: false,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (Response.ok) {
+        // const Data = await Response.json();
+        // console.log("Profile Change", Data);
+        alert("Your Password has been changed");
+        newInputPassword = "";
+      } else {
+        const Data = await Response.json();
+        let errMessage = "Authentication Failed !";
+        if (Data && Data.error && Data.error.message) {
+          errMessage = Data.error.message;
+        }
+        throw new Error(errMessage);
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
   return (
     <Fragment>
       <div className=" flex items-center justify-center mt-40">
@@ -11,14 +51,20 @@ const Profile = () => {
           <label className="block text-gray-600 mb-1 text-center">
             New Password
           </label>
-          <input
-            type="password"
-            placeholder="<PASSWORD>"
-            className="w-full border p-2 rounded focus:outline-none focus:ring focus:border-blue-300"
-          />
-          <button className="w-full bg-blue-500 text-white py-2 mt-4 rounded hover:bg-blue-600 focus:outline-none focus:ring">
-            Change Password
-          </button>
+          <form onSubmit={SubmitHandler}>
+            <input
+              type="password"
+              placeholder="<PASSWORD>"
+              ref={newPassword}
+              className="w-full border p-2 rounded focus:outline-none focus:ring focus:border-blue-300"
+            />
+            <button
+              className="w-full bg-blue-500 text-white py-2 mt-4 rounded hover:bg-blue-600 focus:outline-none focus:ring"
+              type="Submit"
+            >
+              Change Password
+            </button>
+          </form>
         </div>
       </div>
     </Fragment>
